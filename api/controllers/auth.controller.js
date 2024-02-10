4920da23ffb39083d5ecc26e0dc77b14df0dc23b
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-10 20:13:38 +0530

    created signup route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req,res) => {
+    const { username, email, password } = req.body;
+
+    // safety check for valid fields
+    if(!username || !email || !password || username === "" || password === "" || email === "" ){
+        return res.status(400).json({message: "All fields are required"})
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = User({
+        username,
+        email,
+        password : hashedPassword
+    })
+
+    try {
+        await newUser.save()
+        res.json("Signup successful")
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,14 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config()
 mongoose.connect(process.env.MONGO)
 .then(()=>console.log('mongodb is connected'))
 .catch((err)=>console.log(err))
 
 const app = express()
+app.use(express.json()) // to allow POST request to send json to server
+
 app.listen(3000, ()=>{
     console.log(`Server is running on port 3000!`);
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
