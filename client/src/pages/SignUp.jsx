1efe99868aeff354d30272eb3d7563d74a66e691
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-11 00:43:45 +0530

    created sign up UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 rounded-lg text-white bg-gradient-to-r from-indigo-500 via-purple-500 bg-pink-500">
+              Dev
+            </span>
+            Center
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can signup with your email and password
+            or with Google.
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form action="" className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput type="text" placeholder="name@company.com" id="email" />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500"></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
