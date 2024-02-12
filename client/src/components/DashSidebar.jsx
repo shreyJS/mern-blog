56bdf9bb9a0e30d36ab13be8a784c01fb58af86b
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-12 19:04:23 +0530

    user image upload fun complete

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -30,16 +30,18 @@ export default function DashSidebar() {
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Link to="/dashboard?tab=profile">
+            {/* as="div" is used to avoid nesting <a> tag inside Link's <a> tag */}
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item icon={HiArrowSmRight} classname="cursor-pointer">
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
