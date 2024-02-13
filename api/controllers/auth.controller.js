4b3dabfb9be109035d8ebf1ebf2617b77fd0cd8d
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-14 01:21:58 +0530

    admin fun

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -75,7 +75,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Invalid password"));
     }
     // as password is right, token is created using _id
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
     // no need to send password hence removing it from obtained validUser obj
     // and collecting all other properties using rest operator
     const { password: pass, ...rest } = validUser._doc;
@@ -114,7 +114,7 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email });
     if (user) {
       const token = jwt.sign(
-        { id: user._id},
+        { id: user._id, isAdmin: user.isAdmin},
         process.env.JWT_SECRET
       );
       const { password, ...rest } = user._doc;
@@ -149,7 +149,7 @@ export const google = async (req, res, next) => {
 
       // create a token, generate and send a cookie with rest obj
       const token = jwt.sign(
-        { id: newUser._id },
+        { id: newUser._id, isAdmin: newUser.isAdmin},
         process.env.JWT_SECRET
       );
       const { password, ...rest } = newUser._doc;
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -27,6 +27,11 @@ const userSchema = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
     },
+    // not accessible from UI, done manually
+    isAdmin:{
+      type: Boolean,
+      default:false,
+    }
   },
   { timestamps: true }
 );
