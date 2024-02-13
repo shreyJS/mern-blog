91966f731b4c99ebfe6c551309abd8917552940d
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-14 00:20:07 +0530

    delete API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,9 @@
-import express from 'express'
-import { test } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
-import { updateUser } from '../controllers/user.controller.js';
+import express from "express";
+import { test, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
-router.get('/test', test)
-router.put('/update/:userId', verifyToken,updateUser);
-
-export default router
+router.get("/test", test);
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken,deleteUser);
+export default router;
