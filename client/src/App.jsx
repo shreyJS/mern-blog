26392c843f45e34b68ea921c34cc3333cf7a3956
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-14 01:58:01 +0530

    create post page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import {
 } from "./pages/index";
 import { Header, FooterComp } from "./components/index";
 import PrivateRoute from "./components/PrivateRoute"
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
 // routing
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
